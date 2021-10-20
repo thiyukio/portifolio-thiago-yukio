a32346c7d88b08b4157f60e0ffc5c0d2bcb43d1a
Thiago Yukio <thiyukio@gmail.com>
2021-10-20 16:32:18 -0300

    primeiros testes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
+import Layout from '../components/layout';
+import Image from 'next/image';
+import spook from '../static/spooktober11.png';
+
 function Home() {
-    return <div>Home 2</div>
+    return(
+        <Layout>
+        <body>
+        <nav>
+            <a href='./'>thiago yukio</a>
+            <a>galeria</a>
+            <a>contato</a>
+        </nav>
+        <div> 
+        <Image
+            src={spook}
+        />
+        </div>
+        </body>
+        </Layout>
+    )
 }
 
 export default Home
