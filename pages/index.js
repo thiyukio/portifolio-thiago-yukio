8747ee637585297d70d6f58822e4b55e5814c117
Thiago Yukio <thiyukio@gmail.com>
2021-10-22 10:32:15 -0300

    title

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ import Image from 'next/image'
 function Home() {
     return(
         <div>
-            <Head
-            title={'Thiago Yukio'}            
-            />
+            <Head>
+            <title>Thiago Yukio</title>
+            </Head>
             <Image
                 src='/images/spooktober7.png'
                 width={210}
